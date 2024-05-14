ffc1b83a77076da0b183a7148d773d8f09aad6ce
Andrea  Saggiomo <saggiomoandrea@gmail.com>
2024-05-14 10:53:18 +0000

    adding some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// create principal variables 
 
 let playerText = document.getElementById('playerText')
 let restartBtn = document.getElementById('restartBtn')
@@ -5,11 +6,15 @@ let boxes = Array.from(document.getElementsByClassName('box'))
 
 let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
 
+
+// creating functionality for the game 
 const O_TEXT = "O"
 const X_TEXT = "X"
 let currentPlayer = X_TEXT
 let spaces = Array(9).fill(null)
 
+
+// creating the function to start the game 
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
@@ -17,15 +22,15 @@ const startGame = () => {
 function boxClicked(e) {
     const id = e.target.id
 
-    if(!spaces[id]){
+    if (!spaces[id]) {
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
 
-        if(playerHasWon() !==false){
+        if (playerHasWon() !== false) {
             playerText.innerHTML = `${currentPlayer} has won!`
             let winning_blocks = playerHasWon()
 
-            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            winning_blocks.map(box => boxes[box].style.backgroundColor = winnerIndicator)
             return
         }
 
@@ -33,23 +38,25 @@ function boxClicked(e) {
     }
 }
 
+// creating the array fot the combos of the winning combinations
+
 const winningCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
 function playerHasWon() {
     for (const condition of winningCombos) {
         let [a, b, c] = condition
 
-        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
-            return [a,b,c]
+        if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a, b, c]
         }
     }
     return false
@@ -57,12 +64,14 @@ function playerHasWon() {
 
 restartBtn.addEventListener('click', restart)
 
+
+// add the function to resart the game 
 function restart() {
     spaces.fill(null)
 
-    boxes.forEach( box => {
+    boxes.forEach(box => {
         box.innerText = ''
-        box.style.backgroundColor=''
+        box.style.backgroundColor = ''
     })
 
     playerText.innerHTML = 'Tic Tac Toe'
